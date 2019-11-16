d8e3e350f9917c2900e68af310bc674134526adc
Robin Linacre <robinlinacre@hotmail.com>
2019-11-16 12:25:44 +0000

    Create glossary.py

---
+++ glossary.py
@@ -0,0 +1,7 @@
+glossary = {}
+
+definition = """
+'Exchange' is shorthand for a 'Shuffle Exchange. This means that Spark is sending data between executors, probably across the network. This is more often described as merely a 'shuffle'.  This happens between jobs. 
+"""
+
+glossary["exchange"] = definition
